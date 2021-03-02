1c858543fbe58e214a31a8fb97bdfdc016dd49a6
xiaobin.lin <xiaobin.lin@qlchat.com>
2021-03-02 11:16:39 +0800

    数据库sql

--- db.sql
+++ db.sql
@@ -11,11 +11,28 @@
  Target Server Version : 50081
  File Encoding         : 65001
 
- Date: 07/02/2021 16:14:04
+ Date: 02/03/2021 11:13:14
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bili_user_follower
+-- ----------------------------
+DROP TABLE IF EXISTS `bili_user_follower`;
+CREATE TABLE `bili_user_follower`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `mid` bigint(20) NULL DEFAULT NULL COMMENT '用户ID',
+  `following` bigint(20) NULL DEFAULT NULL COMMENT '关注别人数',
+  `whisper` bigint(20) NULL DEFAULT NULL COMMENT 'whisper',
+  `black` bigint(20) NULL DEFAULT NULL COMMENT '黑名单数',
+  `follower` bigint(20) NULL DEFAULT NULL COMMENT '关注数',
+  `gmt_create` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY USING BTREE (`id`),
+  INDEX `idx_create_time_mid` USING BTREE(`gmt_create`, `mid`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for wechat_app_config
@@ -27,7 +44,7 @@ CREATE TABLE `wechat_app_config`  (
   `app_secret` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'APPSECRET',
   `app_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '公众号名称',
   `app_type` enum('subscribe','service') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '公众号类型，subscribe-订阅号，service-服务号',
-  `token` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '调用接口凭证',
+  `access_token` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '调用接口凭证',
   `expires_in` datetime NULL DEFAULT NULL COMMENT 'token 过期时间',
   `gmt_create` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gmt_modified` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -59,6 +76,8 @@ CREATE TABLE `wechat_user_info`  (
   `subscribe_scene` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '返回用户关注的渠道来源，ADD_SCENE_SEARCH-公众号搜索，ADD_SCENE_ACCOUNT_MIGRATION-公众号迁移，ADD_SCENE_PROFILE_CARD-名片分享，ADD_SCENE_QR_CODE-扫描二维码，ADD_SCENE_PROFILE_LINK-图文页内名称点击，ADD_SCENE_PROFILE_ITEM-图文页右上角菜单，ADD_SCENE_PAID-支付后关注，ADD_SCENE_WECHAT_ADVERTISEMENT-微信广告，ADD_SCENE_OTHERS-其他',
   `qr_scene` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '二维码扫码场景',
   `qr_scene_str` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '二维码扫码场景描述',
+  `gmt_create` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY USING BTREE (`id`),
   UNIQUE INDEX `uk_app_user` USING BTREE(`app_id`, `openid`),
   INDEX `idx_openid` USING BTREE(`openid`)
